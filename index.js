a079ca89a15e01ea55a570ea0c89e4ae97c2fcc5
analaurarufino <analaurarufinosouza3@gmail.com>
2023-05-23 22:02:51 -0300

    reports

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import "dotenv/config";
-import readline from "node:readline/promises"
+import readline from "node:readline/promises";
 
-import {
-  connection,
-  Produtos,
-} from "./tabelas/index.js"
+import { connection, Produtos } from "./tabelas/index.js";
 
 const terminal = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
 const legenda = {
   0: "desconectar",
@@ -17,97 +14,130 @@ const legenda = {
   2: "Pesquisar por nome:",
   3: "Inserir Produto:",
   4: "Remover Produto:",
-}
+  5: "Produtos por Categoria:",
+  6: "Produtos feitos Mari:",
+};
+
+const category = {
+  0: "comida",
+  1: "limpeza",
+  2: "utensilio",
+  3: "brinquedo",
+  4: "perfumaria",
+  5: "eletrônico",
+  6: "mídia",
+};
 
 const init = async () => {
-  await connection.connect()
+  await connection.connect();
 
   while (1) {
     const func = await terminal.question(
       `Ola, selecione uma das funções abaixo para continuar\n` +
-      `0: ${legenda[0]}\n` +
-      `1: ${legenda[1]}\n` +
-      `2: ${legenda[2]}\n` +
-      `3: ${legenda[3]}\n` +
-      `4: ${legenda[4]}\n`
-    )
+        `0: ${legenda[0]}\n` +
+        `1: ${legenda[1]}\n` +
+        `2: ${legenda[2]}\n` +
+        `3: ${legenda[3]}\n` +
+        `4: ${legenda[4]}\n` +
+        `5: ${legenda[5]}\n` +
+        `6: ${legenda[6]}\n`
+    );
 
-    let end = false
+    let end = false;
 
     switch (func) {
-    case "0":
-      console.log("logout")
-      end = true
-      break
+      case "0":
+        console.log("logout");
+        end = true;
+        break;
 
-    case "1": {
-      const resultado = await Produtos.listAll()
+      case "1": {
+        const resultado = await Produtos.listAll();
 
-      console.table(resultado)
+        console.table(resultado);
 
-      break
-    }
-    case "2": {
-      const answer = await terminal.question("Digite o nome do produto desejado: ")
+        break;
+      }
+      case "2": {
+        const answer = await terminal.question(
+          "Digite o nome do produto desejado: "
+        );
 
-      const resultado = await Produtos.search(answer)
-      console.table(resultado["0"])
+        const resultado = await Produtos.search(answer);
+        console.table(resultado["0"]);
 
-      break
-    }
-
-    case "3": {
-      const nome = await terminal.question("Digite o nome do seu produto: ")
-
-      const codigo = await terminal.question("Digite o codigo do seu produto: ")
-
-      const categoria = await terminal.question("Digite a categoria do seu produto: ")
-
-      const preco = await terminal.question("Digite o preço do seu produto: ")
-
-      const qtdEstoque = await terminal.question("Digite a quantitade presente no estoque: ")
-
-      const mari = await terminal.question("Foi feito em Mari? (S/N) ")
-
-      let obj = {
-        nome,
-        codigo,
-        categoria,
-        preco,
-        qtdEstoque,
-        mari
+        break;
       }
 
-      console.table([obj])
+      case "3": {
+        const nome = await terminal.question("Digite o nome do seu produto: ");
 
-      await Produtos.insert(
-        nome,
-        codigo,
-        categoria,
-        preco,
-        qtdEstoque,
-        mari
-      )
+        const codigo = await terminal.question(
+          "Digite o codigo do seu produto: "
+        );
+        console.log(category);
+        const choice = await terminal.question(
+          "Digite a categoria do seu produto: "
+        );
+        if (isNaN(choice) || choice > 6 || choice < 0) {
+          console.log("Erro: Categoria inexistente\n");
+          break;
+        }
+        const categoria = category[choice];
+        const preco = await terminal.question(
+          "Digite o preço do seu produto: "
+        );
 
-      console.log("inserido.")
-      break
+        const qtdEstoque = await terminal.question(
+          "Digite a quantitade presente no estoque: "
+        );
+
+        const mari = await terminal.question("Foi feito em Mari? (S/N) ");
+
+        let obj = {
+          nome,
+          codigo,
+          categoria,
+          preco,
+          qtdEstoque,
+          mari,
+        };
+
+        console.table([obj]);
+
+        await Produtos.insert(nome, codigo, categoria, preco, qtdEstoque, mari);
+
+        console.log("inserido.");
+        break;
+      }
+
+      case "4": {
+        const answer = await terminal.question(
+          "Digite o código do produto a ser removido: "
+        );
+        await Produtos.remove(answer);
+        break;
+      }
+      case "5": {
+        const resultado = await Produtos.groupByCategory();
+        console.table(resultado);
+        break;
+      }
+      case "6": {
+        const resultado = await Produtos.productMadeInMari();
+        console.table(resultado);
+        break;
+      }
+
+      default:
+        break;
     }
 
-    case "4": {
-      const answer = await terminal.question("Digite o código do produto a ser removido: ")
-      await Produtos.remove(answer)
-      break
-    }
-
-    default:
-      break
-    }
-
-    if (end) break
+    if (end) break;
   }
 
-  connection.end()
-  terminal.close()
-}
+  connection.end();
+  terminal.close();
+};
 
-init()
+init();
--- tabelas/produto.js
+++ tabelas/produto.js
@@ -33,37 +33,49 @@ export default class Produtos {
     });
   }
 
+  static groupByCategory() {
+    return connection.query({
+      sql: "SELECT categoria, COUNT(*) AS qtd_produtos FROM Produtos GROUP BY categoria",
+    });
+  }
+
+  static productMadeInMari() {
+    return connection.query({
+      sql: "SELECT nome_produto FROM Produtos WHERE feito_em_Mari = 1",
+    });
+  }
+
   static insert(nome, codigo, categoria, preco, qtdEstoque, feitoEmMari) {
     if (isNaN(Number(codigo))) {
-      throw new Error("'codigo' must be a number")
+      throw new Error("'codigo' must be a number");
     }
 
     if (isNaN(Number(preco))) {
-      throw new Error("'preco' must be a number")
+      throw new Error("'preco' must be a number");
     }
 
     if (isNaN(Number(qtdEstoque))) {
-      throw new Error("'qtdEstoque' must be a number")
+      throw new Error("'qtdEstoque' must be a number");
     }
 
     const nome_sql = connection.escape(nome);
     const categoria_sql = connection.escape(categoria);
 
-    feitoEmMari = /^S.*/i.test(feitoEmMari) ? 1 : 0
+    feitoEmMari = /^S.*/i.test(feitoEmMari) ? 1 : 0;
 
     return connection.query({
       sql: `INSERT INTO Produtos
       (cod_produto, nome_produto, preco, categoria, qtd_estoque, feito_em_Mari)
       VALUES
       (${codigo}, ${nome_sql}, ${preco}, ${categoria_sql}, ${qtdEstoque}, ${feitoEmMari})`,
-    })
+    });
   }
 
   static remove(codigo) {
-    const cod_produto = Number(codigo)
+    const cod_produto = Number(codigo);
 
     if (isNaN(cod_produto)) {
-      throw new Error(`'codigo' must be a number.`)
+      throw new Error(`'codigo' must be a number.`);
     }
 
     return connection.query({
