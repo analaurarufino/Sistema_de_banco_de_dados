68e1e2d1622390fa35246a41d3b756ff6d65c907
Felipe Carlan <86426372+lemoslemos@users.noreply.github.com>
2023-05-24 23:31:59 -0300

    progresso

--- index.js
+++ index.js
@@ -1,113 +1,372 @@
 import "dotenv/config";
-import readline from "node:readline/promises"
+import inquirer from "inquirer";
 
 import {
   connection,
   Produtos,
 } from "./tabelas/index.js"
 
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
-
-const legenda = {
-  0: "desconectar",
-  1: "Lista todos os produtos",
-  2: "Pesquisar por nome:",
-  3: "Inserir Produto:",
-  4: "Remover Produto:",
-}
+const categorias = [
+  { name: "Frutas", value: "frutas" },
+  { name: "Legumes", value: "legumes" },
+  { name: "Carnes", value: "carnes" },
+  { name: "Bebidas", value: "bebidas" },
+  { name: "Limpeza", value: "limpeza" },
+  { name: "Laticínios", value: "laticinios" },
+  { name: "Padaria", value: "padaria" },
+  { name: "Higiene Pessoal", value: "higiene_pessoal" },
+  { name: "Eletrônicos", value: "eletronicos" },
+  { name: "Congelados", value: "congelados" },
+  { name: "Grãos e Cereais", value: "graos_e_cereais" },
+  { name: "Bebidas Alcoólicas", value: "bebidas_alcoolicas" },
+  { name: "Cuidados com Animais", value: "cuidados_com_animais" },
+  { name: "Pães", value: "paes" },
+  { name: "Açougue", value: "acougue" },
+  { name: "Hortaliças", value: "hortalicas" },
+  { name: "Doces e Sobremesas", value: "doces_e_sobremesas" },
+  { name: "Artigos de Casa", value: "artigos_de_casa" },
+  { name: "Produtos de Limpeza", value: "produtos_de_limpeza" },
+  { name: "Cereais Matinais", value: "cereais_matinais" },
+  { name: "Bebidas Quentes", value: "bebidas_quentes" },
+  { name: "Utensílios de Cozinha", value: "utensilios_de_cozinha" },
+  { name: "Produtos de Beleza", value: "produtos_de_beleza" },
+]
 
 const init = async () => {
   await connection.connect()
 
   while (1) {
-    const func = await terminal.question(
-      `Ola, selecione uma das funções abaixo para continuar\n` +
-      `0: ${legenda[0]}\n` +
-      `1: ${legenda[1]}\n` +
-      `2: ${legenda[2]}\n` +
-      `3: ${legenda[3]}\n` +
-      `4: ${legenda[4]}\n`
-    )
+    const func = await inquirer.prompt([{
+      type: "list",
+      name: "selected_function",
+      message: "Selecione uma das funções abaixo para continuar:",
+      choices: [
+        { name: "Desconectar", value: 0 },
+        { name: "Listar todos produtos", value: 1 },
+        { name: "Pesquisar produto por nome", value: 2 },
+        { name: "Inserir produto", value: 3 },
+        { name: "Remover produto", value: 4 },
+        { name: "Alterar produto", value: 5 },
+      ]
+    }])
+      .then(answers => answers.selected_function)
 
-    let end = false
+
+    if (func === 0) break
 
     switch (func) {
-    case "0":
-      console.log("logout")
-      end = true
-      break
+    case 1: {
+      try {
+        const result = await Produtos.listAll()
 
-    case "1": {
-      const resultado = await Produtos.listAll()
-
-      console.table(resultado)
-
-      break
-    }
-    case "2": {
-      const answer = await terminal.question("Digite o nome do produto desejado: ")
-
-      const resultado = await Produtos.search(answer)
-      console.table(resultado["0"])
-
-      break
-    }
-
-    case "3": {
-      const nome = await terminal.question("Digite o nome do seu produto: ")
-
-      const codigo = await terminal.question("Digite o codigo do seu produto: ")
-
-      const categoria = await terminal.question("Digite a categoria do seu produto: ")
-
-      const preco = await terminal.question("Digite o preço do seu produto: ")
-
-      const qtdEstoque = await terminal.question("Digite a quantitade presente no estoque: ")
-
-      const mari = await terminal.question("Foi feito em Mari? (S/N) ")
-
-      let obj = {
-        nome,
-        codigo,
-        categoria,
-        preco,
-        qtdEstoque,
-        mari
+        console.table(result)
+      } catch (e) {
+        console.error("[Erro] Listar", e)
       }
 
-      console.table([obj])
+      break
+    }
+    case 2: {
+      const prod_name = await inquirer.prompt({
+        name: "prod_name",
+        message: "Digite o nome do produto desejado:",
+        type: "input"
+      })
+        .then(answer => answer.prod_name)
 
-      await Produtos.insert(
-        nome,
-        codigo,
-        categoria,
-        preco,
-        qtdEstoque,
-        mari
-      )
+      try {
+        const resultado = await Produtos.search(prod_name)
 
-      console.log("inserido.")
+        console.table([resultado["0"]])
+      } catch (e) {
+        console.error("[Erro] Pesquisar", e)
+      }
+
+
       break
     }
 
-    case "4": {
-      const answer = await terminal.question("Digite o código do produto a ser removido: ")
-      await Produtos.remove(answer)
+    case 3: {
+      const nome_produto = await inquirer.prompt({
+        name: "nome_produto",
+        message: "Digite o nome do seu produto:",
+        type: "input",
+        validate: (nome_produto) => {
+          const status = Produtos.validate({ nome_produto })
+
+          if (status !== true)
+            return status
+
+          return true
+        }
+      })
+        .then(answer => answer.nome_produto)
+
+      const cod_produto = await inquirer.prompt({
+        name: "cod_produto",
+        message: "Digite o codigo do seu produto:",
+        type: "number",
+        validate: (cod_produto) => new Promise(async res => {
+          const status_cod = Produtos.validate({ cod_produto })
+
+          if (status_cod !== true)
+            return res(status_cod)
+
+          const resp = await Produtos.get(cod_produto)
+
+          if (resp.length)
+            return res("Esse codigo de produto ja esta sendo usado")
+
+          return res(true)
+        })
+      })
+        .then(answer => answer.cod_produto)
+
+      const categoria = await inquirer.prompt({
+        name: "categoria",
+        message: "Digite a categoria do seu produto:",
+        type: "list",
+        choices: categorias,
+        loop: false,
+      })
+        .then(answer => answer.categoria)
+
+      const preco = await inquirer.prompt({
+        name: "preco",
+        message: "Digite o preço do seu produto:",
+        type: "number",
+        validate: preco => {
+          const status_preco = Produtos.validate({ preco })
+
+          if (status_preco !== true)
+            return status_preco
+
+          return true
+        }
+      })
+        .then(answer => answer.preco)
+
+      const qtd_estoque = await inquirer.prompt({
+        name: "qtd_estoque",
+        message: "Digite a quantitade presente no estoque:",
+        type: "number",
+        validate: (qtd_estoque) => {
+          const status_estoque = Produtos.validate({ qtd_estoque })
+
+          if (status_estoque !== true)
+            return status_estoque
+
+          return true
+        }
+      })
+        .then(answer => answer.qtd_estoque)
+
+      const feito_em_Mari = await inquirer.prompt({
+        name: "feito_em_Mari",
+        message: "Foi feito em Mari?",
+        type: "confirm"
+      })
+        // convert bool to int
+        .then(answer => ~~answer.feito_em_Mari)
+
+      let to_insert = {
+        nome_produto,
+        cod_produto,
+        categoria,
+        preco,
+        qtd_estoque,
+        feito_em_Mari
+      }
+
+      console.table([to_insert])
+
+      try {
+        await Produtos.insert(to_insert)
+
+        console.log("inserido.")
+      } catch (e) {
+        console.error("[Erro] Inserir", e)
+      }
+
       break
+    }
+
+    case 4: {
+      const codigo_produto = await inquirer.prompt({
+        name: "codigo_produto",
+        message: "Digite o código do produto a ser removido:",
+        type: "number",
+        validate: (codigo_produto) => {
+          const status = Produtos.validate({ codigo_produto })
+
+          if (status !== true)
+            return status
+
+          return true
+        },
+      })
+        .then(answer => answer.codigo_produto)
+
+      try {
+        const result = await Produtos.remove(codigo_produto)
+
+        if (result.affectedRows === 1)
+          console.log("Produto removido")
+        else
+          console.log("Produto nao encontrado")
+      } catch (e) {
+        console.error("[Erro] Remover", e)
+      }
+
+      break
+    }
+
+    case 5: {
+      const cod_produto = await inquirer.prompt({
+        name: "cod_produto",
+        message: "Digite o codigo do produto que você quer alterar:",
+        type: "number",
+        validate: cod_produto => new Promise(async res => {
+          const status = Produtos.validate({ cod_produto })
+
+          if (status !== true)
+            return res(status)
+
+          const resp = await Produtos.get(cod_produto)
+
+          if (!resp.length)
+            return res("nao ha nenhum produto com esse codigo")
+
+          return res(true)
+        })
+      })
+        .then(answer => answer.cod_produto)
+
+      const resp = await Produtos.get(cod_produto)
+
+      console.table(resp)
+
+      const to_alter = {}
+
+      const nome_produto = await inquirer.prompt([
+        {
+          name: "decision",
+          message: "Deseja alterar o nome do produto?",
+          type: "confirm",
+        }, {
+          name: "nome_produto",
+          message: "Insira o novo nome do produto:",
+          type: "input",
+          when: a => {
+            return a.decision
+          },
+          validate: nome_produto => {
+            const status = Produtos.validate({ nome_produto })
+
+            return status !== true ? status : true
+          }
+        }
+      ])
+        .then(answers => answers.nome_produto)
+
+      to_alter.nome_produto = nome_produto
+
+      const categoria = await inquirer.prompt([
+        {
+          name: "decision",
+          message: "Deseja alterar a categoria do produto?",
+          type: "confirm",
+        },
+        {
+          name: "categoria",
+          message: "Escolha a nova categoria do produto:",
+          type: "list",
+          choices: categorias,
+          when: a => a.decision,
+        }
+      ])
+        .then(answers => answers.categoria)
+
+      to_alter.categoria = categoria
+
+      const preco = await inquirer.prompt([
+        {
+          name: "decision",
+          message: "Deseja alterar o preco do produto?",
+          type: "confirm"
+        },
+        {
+          name: "preco",
+          message: "Insira o novo preco do produto:",
+          type: "number",
+          when: a => a.decision,
+          validate: preco => {
+            const status = Produtos.validate({ preco })
+
+            return status !== true ? status : true
+          },
+        }
+      ])
+        .then(answers => answers.preco)
+
+      to_alter.preco = preco
+
+      const qtd_estoque = await inquirer.prompt([
+        {
+          name: "decision",
+          message: "Deseja alterar a quantidade em estoque?",
+          type: "confirm"
+        },
+        {
+          name: "qtd_estoque",
+          message: "Insira a nova quantidade em estoque:",
+          type: "number",
+          when: a => a.decision,
+          validate: qtd_estoque => {
+            const status = Produtos.validate({ qtd_estoque })
+
+            return status !== true ? status : true
+          },
+        }
+      ])
+        .then(answers => answers.qtd_estoque)
+
+      to_alter.qtd_estoque = qtd_estoque
+
+      const feito_em_Mari = await inquirer.prompt([
+        {
+          name: "decision",
+          message: "Deseja alterar se o produto foi feito em Mari?",
+          type: "confirm"
+        }
+      ])
+        .then(answers => {
+          const to_negate = answers.decision
+
+          const original = resp['0'].feito_em_Mari
+
+          return ~~(to_negate ? !original : original)
+        })
+
+      to_alter.feito_em_Mari = feito_em_Mari
+
+      try {
+        await Produtos.alter(Object.assign({ cod_produto }, to_alter))
+      } catch (e) {
+        console.error("[Erro] Alterar", e)
+      }
+
+      const altered = await Produtos.get(cod_produto)
+
+      console.table(altered)
     }
 
     default:
       break
     }
-
-    if (end) break
   }
 
   connection.end()
-  terminal.close()
 }
 
 init()
--- tabelas/produto.js
+++ tabelas/produto.js
@@ -1,22 +1,144 @@
 import connection from "./connection.js";
 
+const isString = s => typeof s === 'string' || s instanceof String
+
+const validate_nome = nome_produto => {
+  if (nome_produto === undefined)
+    return true
+
+  if (nome_produto == null)
+    return "nome_produto nao pode ser NULL"
+
+  if (!isString(nome_produto))
+    return "nome_produto precisa ser uma string"
+
+  if (nome_produto.length === 0)
+    return "nome_produto nao pode ser uma string vazia"
+
+  if (nome_produto.length > 255)
+    return "nome_produto precisa ter menos que 255 caracteres"
+
+  return true
+}
+
+const validate_codigo = cod_produto => {
+  if (cod_produto === undefined)
+    return true
+
+  if (cod_produto === null)
+    return "cod_produto nao pode ser NULL"
+
+  if (!Number.isInteger(cod_produto))
+    return "cod_produto precisa ser um inteiro"
+
+  return true
+}
+
+const validate_categoria = categoria => {
+  if (categoria === undefined)
+    return true
+
+  if (categoria === null)
+    return "categoria nao pode ser NULL"
+
+  if (!isString(categoria))
+    return "categoria precisa ser uma string"
+
+  if (categoria.length === 0)
+    return "categoria nao pode ser uma string vazia"
+
+  if (categoria.length > 255)
+    return "categoria precisa ter menos que 255 caracteres"
+
+  return true
+}
+
+const validate_preco = preco => {
+  if (preco === undefined)
+    return true
+
+  if (preco === null)
+    return "preco nao pode ser NULL"
+
+  if (isNaN(preco))
+    return "preco precisa ser um numero"
+
+  if (preco <= 0)
+    return "preco precisa ser maior que zero"
+
+  return true
+}
+
+const validate_estoque = qtd_estoque => {
+  if (qtd_estoque === undefined)
+    return true
+
+  if (qtd_estoque === null)
+    return "qtd_estoque nao pode ser NULL"
+
+  if (!Number.isInteger(qtd_estoque))
+    return "qtd_estoque precisa ser um inteiro"
+
+  if (qtd_estoque < 0)
+    return "qtd_estoque nao pode ser negativo"
+
+  return true
+}
+
+const validate_mari = feito_em_Mari => {
+  if (feito_em_Mari === undefined)
+    return true
+
+  if (feito_em_Mari === null)
+    return "feito_em_Mari nao pode ser NULL"
+
+  if (feito_em_Mari !== 0 && feito_em_Mari !== 1)
+    return "feito_em_Mari precisa ser ou 0 ou 1"
+
+  return true
+}
+
 export default class Produtos {
   static createTable() {
     return connection
       .query({
         sql: `CREATE TABLE Produtos (
-        id INT AUTO_INCREMENT NOT NULL,
-        UNIQUE KEY (id),
-        cod_produto INT NOT NULL,
-        nome_produto VARCHAR(255) NOT NULL,
-        preco FLOAT NOT NULL,
-        categoria VARCHAR(255),
-        qtd_estoque INT NOT NULL DEFAULT 0,
-        feito_em_Mari BOOL NOT NULL DEFAULT 0,
-        PRIMARY KEY (cod_produto)
-      );`,
+          id INT AUTO_INCREMENT NOT NULL,
+          UNIQUE KEY (id),
+          cod_produto INT NOT NULL,
+          PRIMARY KEY (cod_produto),
+          nome_produto VARCHAR(255) NOT NULL,
+          preco FLOAT NOT NULL,
+          categoria VARCHAR(255) NOT NULL,
+          qtd_estoque INT NOT NULL DEFAULT 0,
+          feito_em_Mari BOOL NOT NULL DEFAULT 0,
+        );`,
       })
-      .then(() => console.log(`table 'Produtos' created.`));
+      .then(() => console.log(`table 'Produtos' created.`))
+  }
+
+  static validate({
+    nome_produto, cod_produto, categoria, preco, qtd_estoque, feito_em_Mari
+  }) {
+    const status_nome = validate_nome(nome_produto)
+    if (status_nome !== true) return status_nome
+
+    const status_cod = validate_codigo(cod_produto)
+    if (status_cod !== true) return status_cod
+
+    const status_categoria = validate_categoria(categoria)
+    if (status_categoria !== true) return status_categoria
+
+    const status_preco = validate_preco(preco)
+    if (status_preco !== true) return status_preco
+
+    const status_estoque = validate_estoque(qtd_estoque)
+    if (status_estoque !== true) return status_estoque
+
+    const status_mari = validate_mari(feito_em_Mari)
+    if (status_mari !== true) return status_mari
+
+    return true
   }
 
   static search(name) {
@@ -33,41 +155,83 @@ export default class Produtos {
     });
   }
 
-  static insert(nome, codigo, categoria, preco, qtdEstoque, feitoEmMari) {
-    if (isNaN(Number(codigo))) {
-      throw new Error("'codigo' must be a number")
-    }
+  static showOne() {
+    return connection.query({
+      sql: "SELECT * FROM Produtos LIMIT 1",
+    });
+  }
 
-    if (isNaN(Number(preco))) {
-      throw new Error("'preco' must be a number")
-    }
+  static insert({
+    nome_produto, cod_produto, categoria, preco, qtd_estoque, feito_em_Mari
+  }) {
+    const status = Produtos.validate({
+      nome_produto, cod_produto, categoria, preco, qtd_estoque, feito_em_Mari
+    })
 
-    if (isNaN(Number(qtdEstoque))) {
-      throw new Error("'qtdEstoque' must be a number")
-    }
+    if (status !== true)
+      throw new Error(status)
 
-    const nome_sql = connection.escape(nome);
-    const categoria_sql = connection.escape(categoria);
-
-    feitoEmMari = /^S.*/i.test(feitoEmMari) ? 1 : 0
+    const escaped_nome = connection.escape(nome_produto);
+    const escaped_categoria = connection.escape(categoria);
 
     return connection.query({
       sql: `INSERT INTO Produtos
       (cod_produto, nome_produto, preco, categoria, qtd_estoque, feito_em_Mari)
       VALUES
-      (${codigo}, ${nome_sql}, ${preco}, ${categoria_sql}, ${qtdEstoque}, ${feitoEmMari})`,
+      (${cod_produto}, ${escaped_nome}, ${preco}, ${escaped_categoria}, ${qtd_estoque}, ${feito_em_Mari})`,
     })
   }
 
-  static remove(codigo) {
-    const cod_produto = Number(codigo)
+  static alter({
+    nome_produto, cod_produto, categoria, preco, qtd_estoque, feito_em_Mari
+  }) {
+    const status = Produtos.validate({
+      nome_produto, cod_produto, categoria, preco, qtd_estoque, feito_em_Mari
+    })
 
-    if (isNaN(cod_produto)) {
-      throw new Error(`'codigo' must be a number.`)
-    }
+    if (status !== true)
+      throw new Error(status)
+
+    const to_update = Object.assign({}, {
+      nome_produto,
+      categoria,
+      preco,
+      qtd_estoque,
+      feito_em_Mari
+    })
+
+    const str = Object.entries(to_update)
+      .filter(e => e[1] !== undefined) // only keys with values
+      .map(e => `${e[0]}=${e[1]}`) // name=value
+      .join(",")
+
+    if (!str.length)
+      return
+
+    connection.query({
+      sql: `UPDATE Produtos SET ${str} WHERE cod_produto = ${cod_produto}`
+    })
+  }
+
+  static remove(cod_produto) {
+    const status = Produtos.validate({ cod_produto })
+
+    if (status !== true)
+      throw new Error(status)
 
     return connection.query({
       sql: `DELETE FROM Produtos WHERE cod_produto = ${cod_produto}`,
-    });
+    })
+  }
+
+  static get(cod_produto) {
+    const status = Produtos.validate({ cod_produto })
+
+    if (status !== true)
+      throw new Error(status)
+
+    return connection.query({
+      sql: `SELECT * FROM Produtos WHERE cod_produto = ${cod_produto}`
+    })
   }
 }
